1a3bb61acc1b0280133a287f7e64792d073e3438
Karishma2802 <Karishmaneha2015@gmail.com>
2020-12-29 21:25:49 +0530

    Checking Employee Attendance_UC1

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,7 +1,12 @@
 public class EmployeeWage
 {
-        public static void main(String[] args)
-        {
-                System.out.println("Welcome to Master Branch");
-        }
+	public static void main(String[] args)
+	{
+		int IS_FULL_TIME=1;
+		int Result=(int)(Math.random()*10)%2;
+		if(Result==IS_FULL_TIME)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent");
+	}
 }
